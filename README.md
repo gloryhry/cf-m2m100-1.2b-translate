30c1d1e32ca23f1e6ecb447ce11c894992f0da79
GloryHRY <glory@hzbz.edu.eu.org>
2024-01-17 17:09:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Cloudflare-Translation 
+
+- 一个将Cloudflare的AI翻译模型转为DeepLx的API接口进行调用。
+
+| Model ID | Descripiton |
+| --- | --- |
+| @cf/meta/m2m100-1.2b | Multilingual encoder-decoder (seq-to-seq) model trained for Many-to-Many multilingual translation <br> **languages**: english, chinese, french, spanish, arabic, russian, german, japanese, portuguese, hindi <br> [More information](https://github.com/facebookresearch/fairseq/tree/main/examples/m2m_100) <br> [Terms and license](https://github.com/facebookresearch/fairseq/blob/main/LICENSE)|
+
+## 部署方式
+
+- 配置cloudflare的Work
+- 把[index.js](./index.js)的内容粘贴到cloudflare的work中
+- 在cloudflare > work > 触发器,配置自定义域名
+  
+## 测试
+
+```bash
+curl -X POST http://<workers_name>.<your_workers_domain>.workers.dev/translate \
+-H "Content-Type: application/json" \
+-d '{
+    "text": "Hello, world!",
+    "source_lang": "EN",
+    "target_lang": "DE"
+}'
+```
+
+### Request Parameters
+
+ - text: string
+ - source_lang: string
+ - target_lang: string
+
+### Response
+
+```bash
+{
+    "alternatives": ["Hallo Welt!"],
+    "code": 200,
+    "data": "Hallo Welt!",
+    "method": "Free",
+    "source_lang": "EN",
+    "target_lang": "DE"
+}
+```
---
+++ index.js
@@ -0,0 +1,150 @@
+// 引入@cloudflare/ai.js
+import { Ai } from './vendor/@cloudflare/ai.js';
+function convertLanguageCodeToName(code) {
+  switch (code.toLowerCase()) {
+    case 'bg':
+      return 'bulgarian';
+    case 'BG':
+      return 'bulgarian';
+    case 'zh':
+      return 'chinese';
+    case 'ZH':
+      return 'chinese';
+    case 'cs':
+      return 'czech';
+    case 'CS':
+      return 'czech';
+    case 'da':
+      return 'danish';
+    case 'DA':
+      return 'danish';
+    case 'nl':
+      return 'dutch';
+    case 'NL':
+      return 'dutch';
+    case 'en':
+      return 'english';
+    case 'EN':
+      return 'english';
+    case 'et':
+      return 'estonian';
+    case 'ET':
+      return 'estonian';
+    case 'fi':
+      return 'finnish';
+    case 'FI':
+      return 'finnish';
+    case 'fr':
+      return 'french';
+    case 'FR':
+      return 'french';
+    case 'de':
+      return 'german';
+    case 'DE':
+      return 'german';
+    case 'el':
+      return 'greek';
+    case 'EL':
+      return 'greek';
+    case 'hu':
+      return 'hungarian';
+    case 'HU':
+      return 'hungarian';
+    case 'it':
+      return 'italian';
+    case 'IT':
+      return 'italian';
+    case 'ja':
+      return 'japanese';
+    case 'JA':
+      return 'japanese';
+    case 'lv':
+      return 'latvian';
+    case 'LV':
+      return 'latvian';
+    case 'lt':
+      return 'lithuanian';
+    case 'LT':
+      return 'lithuanian';
+    case 'pl':
+      return 'polish';
+    case 'PL':
+      return 'polish';
+    case 'pt':
+      return 'portuguese';
+    case 'PT':
+      return 'portuguese';
+    case 'ro':
+      return 'romanian';
+    case 'RO':
+      return 'romanian';
+    case 'ru':
+      return 'russian';
+    case 'RU':
+      return 'russian';
+    case 'sk':
+      return 'slovak';
+    case 'SK':
+      return 'slovak';
+    case 'sl':
+      return 'slovenian';
+    case 'SL':
+      return 'slovenian';
+    case 'es':
+      return 'spanish';
+    case 'ES':
+      return 'spanish';
+    case 'sv':
+      return 'swedish';
+    case 'SV':
+      return 'swedish';
+    default:
+      return 'english';
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    try {
+      // 检查请求方法和Content-Type
+      if (request.method !== 'POST' || request.headers.get('Content-Type') !== 'application/json') {
+        const return_text = 'DeepL Translate Api\nPOST {"text": "have a try", "source_lang": "auto", "target_lang": "ZH"} to /translate \nhttps://github.com/gloryhry/cf-m2m100-1.2b-translate.git';
+        return new Response(return_text, { status: 200 });
+      }
+
+      // 检查请求地址
+      if (request.url.endsWith('/translate')) {
+
+        // 解析请求体
+        const requestBody = await request.json();
+        // console.log(requestBody);
+
+        const ai = new Ai(env.AI);
+        // 调用翻译函数
+        const translationResult = await ai.run('@cf/meta/m2m100-1.2b', {
+          text: requestBody.text,
+          source_lang: convertLanguageCodeToName(requestBody.source_lang), // defaults to english
+          target_lang: convertLanguageCodeToName(requestBody.target_lang)
+        });
+        // console.log(translationResult);
+        // 构建回复
+        const response = {
+          alternatives: [translationResult.translated_text],
+          code: 200,
+          data: translationResult.translated_text,
+          method: 'Free',
+          source_lang: requestBody.source_lang,
+          target_lang: requestBody.target_lang
+        };
+
+        // 返回翻译结果
+        return new Response(JSON.stringify(response), { status: 200, headers: { 'Content-Type': 'application/json' } });
+      } else {
+        return new Response('Not Found', { status: 404 });
+      }
+    } catch (error) {
+      console.error(error);
+      return new Response('Internal Server Error', { status: 500 });
+    }
+  }
+}
